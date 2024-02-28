81aae885dbb00aaf720a00562f38b2e7d411eaf6
shMuscat <160601327+shMuscat@users.noreply.github.com>
2024-02-28 09:07:10 +0900

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);
+        }
+
+        if(Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0);
+        }
+    }
+
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+}
